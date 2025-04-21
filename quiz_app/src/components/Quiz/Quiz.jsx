124f5a68444a3d80df2910bff25f980144fa728b
Sana Ahsan <sanaahsanmallick@gmail.com>
2025-04-21 18:38:55 +0530

    Added

--- quiz_app/src/components/Quiz/Quiz.jsx
+++ quiz_app/src/components/Quiz/Quiz.jsx
@@ -1,18 +1,49 @@
-import React from "react"
+import React, { useState } from "react"
 import './Quiz.css'
+import { data } from "../../assets/data";
+import { useRef } from "react";
 
 const Quiz=()=>{
+   let[index,setIndex]=useState(0);//q1
+   let[question,setQuestion]= useState(data[index]);
+   let[lock,setLock]=useState(false);
+
+   //to show the correct ans after user chooses wrong
+   
+   let Option1= useRef(null);
+   let Option2= useRef(null);
+   let Option3= useRef(null);
+   let Option4= useRef(null);
+
+   let option_array=[Option1,Option2,Option3,Option4];
+
+   const checkAns=(e,ans)=>{
+    if(lock===false){//so that user can choose only one option
+        if(question.ans===ans){
+            e.target.classList.add("correct");
+            setLock(true);
+           }
+           else{
+            e.target.classList.add("wrong");
+            setLock(true);
+           }
+    }
+   }
+
+   
+          
+   
     return(
         <div>
            <div className="container">
             <h1>Quiz App</h1>
             <hr></hr>
-            <h2>Which device is required for the Internet connection?</h2>
+            <h2>{index +1}. {question.question}</h2>
             <ul>
-                <li>Modem</li>
-                <li>Router</li>
-                <li>LAN Cable</li>
-                <li>Pen Drive</li>
+                <li onClick={(e)=>{checkAns(e,1)}}>{question.option1}</li>
+                <li onClick={(e)=>{checkAns(e,2)}}>{question.option2}</li>
+                <li onClick={(e)=>{checkAns(e,3)}}>{question.option3}</li>
+                <li onClick={(e)=>{checkAns(e,4)}}>{question.option4}</li>
             </ul>
             <button>Next</button>
            <div className="index">1 of 5 questions</div>
